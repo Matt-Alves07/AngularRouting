26b05d0e35f1540b42de2e1a50bb88e782f7495f
Matheus Alves <mathalves07.winstore@outlook.com>
2023-01-03 20:51:41 -0300

    Created the base pages, and a menu component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+//Pages
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+
 const routes: Routes = [
-  
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'about', component: SobreComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '404' }
 ];
 
 @NgModule({
