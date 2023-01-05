0b352424498d2584dddccb17fc7e9120fd3b0462
Matheus Alves <mathalves07.winstore@outlook.com>
2023-01-05 17:44:03 -0300

    Created a lazy loading to a component called Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,13 @@ import { SobreComponent } from './pages/sobre/sobre.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'about/:id', component: SobreComponent },
+  { path: 'about', component: SobreComponent, children: [
+    { path: ':id', component: SobreComponent }
+  ]},
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardModule)
+  },
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '404' }
 ];
